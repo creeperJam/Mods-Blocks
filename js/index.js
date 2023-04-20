a6279dd3feb5cef72a7d64ff92544e5b737fd83c
creeperJam <albi04sa@gmail.com>
2023-04-20 22:23:43 +0200

    FINITA RICERCA OGGETTO

--- js/index.js
+++ js/index.js
@@ -17,8 +17,6 @@ const linkApi = "https://minecraft-api.vercel.app/api";
 // fields=%5B%22name%22%2C%22image%22%2C%22stackSize%22%5D --> ["name", "image", "stackSize"] --> campi da visualizzare
 // stackSize=64 --> numero di blocchi per stack
 
-// TODO: aggiungere il grassetto alla parte già scritta
-
 const cercaBlocchi = "/items/";
 let debounceTimeout = 0;
 function cercaBlocco() {
@@ -49,7 +47,7 @@ function cercaBlocco() {
 let currentFocus = -1;
 function suggerimenti(arr) {
     let div = document.getElementById('autocomplete-list');
-    div.innerHTML = "";
+    closeAllLists();
 
     if (arr.length == 0) {
         let suggerimento = document.createElement('DIV');
@@ -66,14 +64,14 @@ function suggerimenti(arr) {
         let n = arr.length > 10 ? 10 : arr.length;
         for (let i = 0; i < n; i++) {
             let inizioStringa = arr[i]['name'].toLowerCase().indexOf(inputNomeBlocco.value.toLowerCase());
-            // console.log(arr[i]['name'])
-            // console.log(inizioStringa + " - InizioStringa");
             let suggerimento = document.createElement('DIV');
             suggerimento.setAttribute("class", "autocomplete-item");
-            suggerimento.setAttribute("value", arr[i]['name']);
             let testoSuggerimento = document.createElement('span');
+            testoSuggerimento.setAttribute("value", arr[i]['name']);
+            if (i == n - 1) testoSuggerimento.setAttribute("style", "border-radius: 0 0 var(--border-radius) var(--border-radius)");
+            else testoSuggerimento.setAttribute("style", "border-radius: 0");
 
-            suggerimento.innerHTML = "<p class='vuoto'></p> <p class='primaColonna'></p>";
+            suggerimento.innerHTML = "<p class='vuoto'></p> <p class='primaColonna' style='color: rgba(0, 0, 0, 0)'>Oggetto: </p>";
             testoSuggerimento.innerHTML += arr[i]['name'].substr(0, inizioStringa);
             testoSuggerimento.innerHTML += "<strong>" + arr[i]['name'].substr(inizioStringa, inputNomeBlocco.value.length) + "</strong>";
             testoSuggerimento.innerHTML += arr[i]['name'].substr(inizioStringa + inputNomeBlocco.value.length,);
@@ -87,54 +85,58 @@ function suggerimenti(arr) {
             div.appendChild(suggerimento);
         }
     }
-
-    function apriPaginaBlocco() {
-        // console.log(this.value);
-        let nomeBlocco = this.value.replace(/\s/g, '+');
-        let link = window.location.href + "risultatoRicerca.html" + "?" + nomeBlocco;
-        window.location.href = link;
-    }
-
-    function closeAllLists() {
-        let div = document.getElementById("autocomplete-list");
-        let children = div.childNodes, n = children.length;
-        for (let i = 0; i < n; i++) {
-            div.removeChild(children[0]);
-        }
-    }
-
-    inputNomeBlocco.addEventListener("keypress", function (e) {
-        let tastoPremuto = e.code;
-
-        if (tastoPremuto == "ArrowDown") {
-            console.log(currentFocus);
-
-            if (currentFocus > -1) div.children[currentFocus].blur();
-            if (currentFocus < 9) currentFocus++;
-            div.children[currentFocus].focus();
-
-            console.log(currentFocus);
-        } else if (tastoPremuto == "ArrowUp") {
-            console.log(currentFocus);
-
-            if (currentFocus > -1) div.children[currentFocus].blur();
-            if (currentFocus > 0) currentFocus--;
-            div.children[currentFocus].focus();
-
-            console.log(currentFocus);
-        } else if (tastoPremuto == "Enter") {
-            e.preventDefault();
-            console.log("INVIO if-case - " + tastoPremuto)
-        } else {
-            console.log("Tasto premuto - " + tastoPremuto)
-        }
-    })
-
-    document.addEventListener("focusout", function (e) {
-        closeAllLists();
-    });
 }
 
+function apriPaginaBlocco() {
+    // console.log(this.value);
+    let nomeBlocco = this.value.replace(/\s/g, '+');
+    let link = window.location.href + "risultatoRicerca.html" + "?" + nomeBlocco;
+    window.location.href = link;
+}
+
+function closeAllLists() {
+    let div = document.getElementById("autocomplete-list");
+    let children = div.childNodes, n = children.length;
+    for (let i = 0; i < n; i++) {
+        div.removeChild(children[0]);
+    }
+}
+
+function blur(element) {
+    element.removeAttribute("class", "spanFocus");
+}
+
+function focus(element) {
+    element.setAttribute("class", "spanFocus");
+}
+
+inputNomeBlocco.addEventListener("keyup", function (e) {
+    let divs = document.getElementById('autocomplete-list').children;
+    let tastoPremuto = e.code;
+
+    // console.log(tastoPremuto);
+
+    if (tastoPremuto == "ArrowDown") {
+        if (currentFocus > -1) blur(divs[currentFocus].children[2]);
+        if (currentFocus < 9) currentFocus++;
+        focus(divs[currentFocus].children[2]);
+    } else if (tastoPremuto == "ArrowUp") {
+        if (currentFocus > -1) blur(divs[currentFocus].children[2]);
+        if (currentFocus > 0) currentFocus--;
+        focus(divs[currentFocus].children[2]);
+    } else if (tastoPremuto == "Enter") {
+        e.preventDefault();
+        // console.log("INVIO if-case - " + tastoPremuto)
+        // console.log(divs[currentFocus].children[2].value);
+        inputNomeBlocco.value = divs[currentFocus].children[2].getAttribute('value');
+    } else {
+        // console.log("Tasto premuto - " + tastoPremuto)
+    }
+})
+
+document.addEventListener("focusout", function (e) {
+    closeAllLists();
+});
 
 document.addEventListener("keypress", function (e) {
     if (e.code == "Enter") {
@@ -142,3 +144,6 @@ document.addEventListener("keypress", function (e) {
         // console.log("INVIO documento - " + e.code)
     }
 })
+
+
+// TODO: Creazione HTML con risultato ricerca
